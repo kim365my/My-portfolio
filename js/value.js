e33f78fac293db4f2323698664202b4bd539ddef
kim365my <kim365my@knou.ac.kr>
2023-03-07 11:55:02 +0900

    완성

--- js/value.js
+++ js/value.js
@@ -1,13 +1,19 @@
-window.onload = function(){
-    // dom 객체 생성
-    const navBtn = document.querySelectorAll(".gnb a");
-    const desTitle = document.querySelector(".des h2");
-    const desLink = document.querySelector(".des p > a");
-    const iframe = document.querySelector(".innerPage iframe");
+// dom 객체 생성
+const gnb = document.querySelectorAll(".gnb li");
+const navList = document.querySelectorAll(".list");
+const navListBtn = document.querySelectorAll(".list a");
+const desTitle = document.querySelector(".des h2");
+const desLink = document.querySelector(".des p > a");
+const iframe = document.querySelector(".innerPage iframe");
 
+// 함수실행
+printContent();
+gnbAndList();
+
+function printContent(){
     // 이벤트 리스너
-    for (let i = 0; i < navBtn.length; i++) {
-        navBtn[i].addEventListener("click", function(e){
+    for (let i = 0; i < navListBtn.length; i++) {
+        navListBtn[i].addEventListener("click", function(e){
             e.preventDefault();
 
             let eText= e.target.text;
@@ -24,6 +30,25 @@ window.onload = function(){
             // 설명을 li에 숨겨서 그것도 가져오게
             // console.dir(e.target);
         })
+    }
+}
 
+function gnbAndList(){
+    const CHECK_CLASS = "check"
+    for (let i = 0; i < gnb.length; i++) {
+        gnb[i].addEventListener("click", function(e){
+            e.preventDefault();
+
+            let className = gnb[i].classList.contains(CHECK_CLASS);
+            if (!className) {
+                for (let index = 0; index < gnb.length; index++) {
+                    gnb[index].classList.remove(CHECK_CLASS);
+                    navList[index].classList.remove(CHECK_CLASS);
+                }
+                gnb[i].classList.add(CHECK_CLASS);
+                navList[i].classList.add(CHECK_CLASS);
+            }
+        })
+        
     }
 }
