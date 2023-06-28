9259dc56fe093dcf9df113d6a92f0076377ea851
kim365my <kim365my@knou.ac.kr>
2023-06-28 17:39:31 +0900

    up

--- module/001_맥도날드/js/script.js
+++ module/001_맥도날드/js/script.js
@@ -8,7 +8,6 @@ noLink.forEach((link) => {
         if(!link.classList.contains("eventNone")) {
             e.preventDefault();
             alertMeg.classList.add("showAlert");
-            alertMeg.classList.remove("hidden");
             window.setTimeout(() => {
                 alertMeg.classList.remove("showAlert");
             }, 2000);
